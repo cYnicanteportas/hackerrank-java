1ef73df2d3dc2e1b650f19b74d5d0f9aad5e8d88
cYnicanteportas <73060019+cYnicanteportas@users.noreply.github.com>
2020-10-20 16:22:41 +0200

    Add files via upload

---
+++ Java Static Initializer Block.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    //Write your code here
+    static Scanner scan = new Scanner(System.in);
+    static int B = scan.nextInt();
+    static int H = scan.nextInt();
+    static boolean flag;
+
+    static {
+        try {
+            if (B > 0 && H > 0) {
+                flag = true;
+            }
+            else {
+                throw new Exception("Breadth and height must be positive");
+            }
+        }
+        catch(Exception e){
+                System.out.println(e);
+            }
+        }
+
+
+
+
+    public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+
+    }//end of main
+
+}//end of class
+
